e3f8164f5b0b9b35bdd79b68c6417c6b22e54948
Kirill <vasolovk56@gmail.com>
2022-11-29 21:50:42 +0300

    Добавлены коды для 2 задач

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿/*Console.WriteLine("Введите первое число: ");
+string a1 = Console.ReadLine();
+int a = Convert.ToInt32(a1);
+Console.WriteLine("Введите второе число: ");
+string b1 = Console.ReadLine();
+int b = Convert.ToInt32(b1);
+
+if (a > b){
+    int max = a;
+    int min = b;
+    Console.WriteLine("max: " + max + "; min: " + min);
+} else if (a < b) {
+    int max = b;
+    int min = a;
+    Console.WriteLine("max: " + max + "; min: " + min);
+} else if (a == b) {
+    Console.WriteLine("Числа равны");
+}*/
+
+/*Console.WriteLine("Введите первое число: ");
+string a1 = Console.ReadLine();
+int a = Convert.ToInt32(a1);
+Console.WriteLine("Введите второе число: ");
+string b1 = Console.ReadLine();
+int b = Convert.ToInt32(b1);
+Console.WriteLine("Введите третье число: ");
+string c1 = Console.ReadLine();
+int c = Convert.ToInt32(c1);
+
+if (a > b){
+    int max = a;
+    if (max > c){
+        max = max;
+        Console.WriteLine("Максимальное: " + max);
+    } else {
+        max = c;
+        Console.WriteLine("Максимальное: " + max);
+    }
+} else {
+    int max = b;
+    if (max > c){
+        max = max;
+        Console.WriteLine("Максимальное: " + max);
+    } else {
+        max = c;
+        Console.WriteLine("Максимальное: " + max);
+    }
+}*/
+
+
+
+
