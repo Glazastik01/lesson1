dfd6ee2f5048b4674300f83aa13c57f7208660cc
Kirill <vasolovk56@gmail.com>
2022-12-09 21:19:03 +0300

    Добавлены задачи к домашке на 07.12.22 и коды с семинара 09.12.22

--- Program.cs
+++ Program.cs
@@ -183,3 +183,293 @@ void GetNumber(int num){
 int num = Convert.ToInt32(Console.ReadLine());
 GetNumber(num);
 */
+
+
+
+
+/*
+int GetNumber(string message){
+
+    bool isCorrect = false;
+    int result = 0;
+
+    while(!isCorrect){
+        
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result) && result != 0 ){
+            isCorrect = true;
+        } else {
+            Console.WriteLine("Ввели некорректное число");
+        }
+    }
+
+    return result;
+}
+
+
+int GetQuarter(int x, int y)
+{
+    if(x > 0 && y > 0)
+        return 1;
+    if(x < 0 && y > 0)
+        return 2;
+    if(x < 0 && y < 0)
+        return 3;
+
+    return 4;
+}
+
+int x = GetNumber("Введите координату х:");
+int y = GetNumber("Введите координату y:");
+int quarter = GetQuarter(x, y);
+
+Console.WriteLine($"Точка с координатами ({x}, {y}) лежит в {quarter} четверти");
+*/
+
+//Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+/*
+string Method(int qua){
+    switch(qua){
+        case 1:
+            return "x = 0 ... +беск\ny = 0 ... +беск";
+            break;
+        case 2:
+            return "x = 0 ... +беск\ny = 0 ... +беск";
+            break;
+        case 3:
+            return "x = 0 ... +беск\ny = 0 ... +беск";
+            break;
+        case 4:
+            return "x = 0 ... +беск\ny = 0 ... +беск";
+            break;
+        default:
+            return "Введите чило от 1 до 4";
+    }
+}
+
+Console.WriteLine($"Диапазон чисел на заданой четверти равна: {Method(1)}");
+*/
+
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+//A (3,6); B (2,1) -> 5,09 
+//A (7,-5); B (1,-1) -> 7,21
+
+/*
+Console.WriteLine("Введите значение X первой точки");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение Y первой точки");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение X второй точки");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение Y второй точки");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double DistXY(int x1, int y1, int x2, int y2){
+
+    double result = 0;
+    result = Math.Sqrt(Math.Pow((x1-x2), 2) + Math.Pow((y1-y2), 2));
+    return result;
+}
+
+Console.WriteLine($"Расстояние между точками = {DistXY(x1,y1,x2,y2)}");
+*/
+
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+//5 -> 1, 4, 9, 16, 25.
+//2 -> 1,4
+
+/*
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= N; i++)
+{
+    Console.Write($"{Math.Pow(i, 2)}, ");
+}
+*/
+
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// Задача 1 из дз на 07.12.22
+/*
+Console.WriteLine("Введите пятизначное число");
+
+try{
+    int num = Convert.ToInt32(Console.ReadLine());
+    int num_orig = num;
+    int rev = 0;
+    while(num > 0){
+        int dig = 0;
+        dig = num % 10;
+        rev = rev * 10 + dig;
+        num = num / 10;
+    }
+    if (num_orig == rev){
+        Console.WriteLine("Число палиндром");
+    } else {
+        Console.WriteLine("Число не палиндром");
+    }
+} catch (Exception ex){
+    Console.WriteLine("Некорректный ввод", ex);
+}
+*/
+
+
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+//Задача 2 из дз на 07.12.22
+
+/*
+Console.WriteLine("Введите значение X первой точки");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение Y первой точки");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение X второй точки");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение Y второй точки");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение Z первой точки");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение Z второй точки");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double DistXY(int x1, int y1, int z1, int x2, int y2, int z2){
+
+    double result = 0;
+    result = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2-y1), 2) + Math.Pow((z2 - z1), 2));
+    return result;
+}
+
+Console.WriteLine($"Расстояние между точками = {DistXY(x1,y1,x2,y2,z1,z2)}");
+*/
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+//Задача 3 из дз на 07.12.22
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+double num_orig = num;
+int x = 0;
+while (x <= num){
+    num_orig = Math.Pow(x, 3);
+    x++;
+    Console.Write(num_orig + ", ");
+}
+
+//Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+//456 -> 3
+//78 -> 2
+//89126 -> 5
+/*
+int GetNumber(string message){
+
+    bool isCorrect = false;
+    int result = 0;
+
+    while(!isCorrect){
+        
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result) && result != 0 ){
+            isCorrect = true;
+        } else {
+            Console.WriteLine("Ввели некорректное число");
+        }
+    }
+
+    return result;
+}
+
+int GetLenght(int number)
+{
+    var count = 0;
+      int num = number;
+    
+    while(num > 0)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
+
+int x = GetNumber("Введите число");
+
+int len = GetLenght(x);
+
+Console.WriteLine($"Число {x} длинной {len}");
+*/
+
+//Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+//4 -> 24 
+//5 -> 120
+/*
+int GetNumber(string message){
+
+    bool isCorrect = false;
+    int result = 0;
+
+    while(!isCorrect){
+        
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result) && result != 0 ){
+            isCorrect = true;
+        } else {
+            Console.WriteLine("Ввели некорректное число");
+        }
+    }
+
+    return result;
+}
+
+int GetFact(int number){
+    
+    int res = 1;
+    for (int i = 1; i <= number; i++){
+        res = i * res;
+    } 
+    return res;
+}
+int x = GetNumber("Введите число");
+int f = GetFact(x);
+Console.WriteLine($"Факториалом числа {x} является {f}");
+*/
+
+//Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+//[1,0,1,1,0,1,0,0]
+/*
+int[] numbers = new int[8];
+Random rnd = new Random();
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = rnd.Next(0, 2);
+}
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write(numbers[i] +" ");
+}
+*/
+/*
+int[] numbers = new int[8];
+Random rnd = new Random();
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = rnd.Next(0, 2);
+}
+string d = String.Join(",", numbers);
+
+Console.WriteLine(d);
+*/
